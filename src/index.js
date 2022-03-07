34a901ca5751aff720f058e381f95bb64d5898a2
unknown <shainnymarcruz345@gmail.com>
2022-03-06 22:09:09 -0200

    enabled

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './routes/App';
-// import reducer from './reducers';
-import './assets/styles/main.scss';
-import './assets/styles/theme.scss';
-
-ReactDOM.render(<App />, document.querySelector('#app'));
--- src/loaders/Skeleton.jsx
+++
@@ -1,7 +0,0 @@
-import '../assets/styles/components/Skeleton.scss';
-const Skeleton = ({ type }) => {
-  const classes = `skeleton ${type}`;
-
-  return <div className={classes}></div>;
-};
-export default Skeleton;
